b2dcd8c28d8bf362924945f769bc609246172e8c
Rehaf1 <160858332+Rehaf1@users.noreply.github.com>
2024-11-14 18:29:19 +0300

    Update Test.java by Tessa

--- DataStructure/src/ds_Project/Test.java
+++ DataStructure/src/ds_Project/Test.java
@@ -1,4 +1,4 @@
-package ds_Project;
+package projectTest1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,75 +8,90 @@ public class Test {
 
 
 
-	public static void main(String[] args) throws IOException {
-		String file = "/Users/rehaf/git/Ds-project/DataStructure/dataset.csv";  // file place
-		BufferedReader reader = null , reader1 = null; 
-		
-		String line ="";
-		String file2 ="/Users/rehaf/git/Ds-project/DataStructure/stop.txt"; 
-		String [] stop = new String[572];
-		int count = 0; 
-		int num = 0 ; 
-		try { 
-		reader1 = new BufferedReader(new FileReader(file2));  // reading the file 
-		
-		while((line = reader1.readLine()) != null  ) {
-			 
-			stop[count++]=" "+ line+ " "; // so that i don't replace the letters in words
-		}stop[count]="\"";
-		 
-		}catch (IOException e1) { 
-			e1.printStackTrace();
-		}finally {
-			reader1.close(); 
-		}
-		
-		
-		LinkedList <String> outerlist = new LinkedList<String>();  
-		try { 
-			reader = new BufferedReader(new FileReader(file));  // reading the file 
-			line = reader.readLine();  // to not read the header of the column 
-			while((line = reader.readLine()) != null && num < 50 ) {
-				num++ ;// to not read the tokens or the vocab or the topic.  
-				LinkedList <String> innerlist = new LinkedList<String>(); 
-				
-				String[]row = line.split(",");  
-				   if (row.length > 1) {
-					  row[1]= row[1].toLowerCase(); 
-					  row[1] =  row[1]; // to remove stop word if its the first word 
-					   // removing all stop words 
-					   for (String str : stop) {       // this is basically a loop for every string in stop
-						   if(str != null)
-						   	row[1]= row[1].replace(str, " ") ;
-					   } 
-					   
-				String[] elements = row[1].split(" ");
-				for (int i =0 ; i< elements.length ; i++) 
-					innerlist.insert(elements[i]);
-					
-				outerlist.insert(row[0], innerlist);
-				   } 
-				
-				}
-				System.out.println();
-				
-				outerlist.print(); 
-			}
-		catch(IOException e)
-		{
-             e.printStackTrace();
-	}
-		finally {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		}
-		
+    public static void main(String[] args) throws IOException {
 
-	}}
+        String file = "C:\\Users\\Hessa\\Downloads\\dataset.csv"; // file place        
+        String file2 = "C:\\Users\\Hessa\\Downloads\\stop.txt";
+
+        String[] stop = new String[572];
+        String line = "";
+        int count = 0;
+        int num = 0;
+
+        BufferedReader reader = null, reader1 = null;
+
+        try {
+            reader1 = new BufferedReader(new FileReader(file2)); // reading the file 
+
+            while ((line = reader1.readLine()) != null) {
+
+                stop[count++] = line.trim(); // without extra spaces
+            }
+            //stop[count] = "\"";
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } finally {
+        	try {
+                reader1.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
 
+        LinkedList < String > outerlist = new LinkedList < String > ();
+        try {
+            reader = new BufferedReader(new FileReader(file)); // reading the file 
+            line = reader.readLine(); // to not read the header of the column 
+            
+            while ((line = reader.readLine()) != null && num < 50) {
+                num++; // to not read the tokens or the vocab or the topic. //but the null comes before them no? 
 
+                LinkedList < String > innerlist = new LinkedList < String > ();
+
+                int firstCommaIndex = line.indexOf(","); //find 1st comma
+                if (firstCommaIndex != -1) {
+                    String id = line.substring(0, firstCommaIndex); // get ID
+                    String content = line.substring(firstCommaIndex + 1); // get content
+
+                    content = content.toLowerCase();
+                    content = content.replaceAll(",", " "); //need?
+                    content = content.replaceAll("-", ""); // remove "-" 
+
+                    
+                    // removing all stop words
+                    for (String str: stop) { // this is basically a loop for every string in stop
+                        if (str != null)
+                            content = content.replaceAll("\\b" + str + "\\b", " "); // so that i don't replace the letters in words
+                    }
+
+                    content = content.replaceAll("[^a-z\\s]", ""); // remove all not letters/spaces
+                    content = content.replaceAll("\\s+", " ").trim(); // replace multiple spaces with a single space and trim spaces in beginning and end
+
+                    String[] elements = content.split(" ");
+                    for (String element: elements)
+                        innerlist.insert(element);
+
+                    outerlist.insert(id, innerlist);
+                }
+
+            }
+
+            System.out.println();
+            outerlist.print();
+            
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+}
